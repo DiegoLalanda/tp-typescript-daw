d27145297f72a4d4dd06d31bb2fa97a0dea0b624
FernandaElola <fernanda.elola@tiendamia.com>
2025-03-30 13:17:28 -0300

    subo puntos del 6 al 10

--- main.ts
+++ main.ts
@@ -43,3 +43,69 @@ const gato: Gato = new Gato("Michi");
 describirAnimal(perro);
 describirAnimal(vaca);
 describirAnimal(gato);
+
+// 6. Crear un Enum DiasSemana
+enum DiasSemana {
+    Lunes = "Lunes",
+    Martes = "Martes",
+    Miércoles = "Miércoles",
+    Jueves = "Jueves",
+    Viernes = "Viernes",
+    Sábado = "Sábado",
+    Domingo = "Domingo"
+}
+
+// 7. Crear una variable que pueda contener número o string
+let entrada: string | number;
+
+// Asignar el string “Messi”
+entrada = "Messi";
+console.log(`Variable entrada:`, entrada); 
+
+// Reemplazar por por el número 10
+entrada = 10;
+console.log(`Variable entrada luego del cambio:`, entrada); 
+
+// 8. Crear clase genérica que implemente la interfaz Fila
+interface Fila<T> {
+    agregar(elemento: T): void;
+    remover(): T | undefined;
+}
+
+class FilaGenerica<T> implements Fila<T> {
+    private elementos: T[] = [];
+
+    agregar(elemento: T): void {
+        this.elementos.push(elemento);
+        console.log(`Elemento agregado:`, elemento); 
+    }
+
+    remover(): T | undefined {
+        const elementoRemovido = this.elementos.shift();
+        console.log(`Elemento removido:`, elementoRemovido);
+        return elementoRemovido;
+    }
+}
+
+// 9. Crear una fila para números, una fila para strings, y una fila para animales
+let filaNumeros: FilaGenerica<number> = new FilaGenerica<number>();
+let filaStrings: FilaGenerica<string> = new FilaGenerica<string>();
+let filaAnimales: FilaGenerica<Animal> = new FilaGenerica<Animal>();
+
+// 10. Agregar elementos a las filas
+filaNumeros.agregar(100);
+filaNumeros.agregar(200);
+filaNumeros.agregar(300);
+
+filaStrings.agregar("manzana");
+filaStrings.agregar("banana");
+filaStrings.agregar("frutilla");
+
+filaAnimales.agregar(perro);
+filaAnimales.agregar(vaca);
+filaAnimales.agregar(gato);
+
+// 10. Remover un elemento de cada fila
+const numeroRemovido = filaNumeros.remover();
+const stringRemovido = filaStrings.remover();
+const animalRemovido = filaAnimales.remover();
